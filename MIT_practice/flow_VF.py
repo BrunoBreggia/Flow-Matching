1b3799998d18e7280c1a98a2a0d0b3a759e50d77
Gisch97 <gkulemeyer@gmail.com>
2025-06-19 22:42:30 -0300

    Agrego lab 2 como paquete: flow

---
+++ MIT_practice/flow_VF.py
@@ -0,0 +1,120 @@
+from abc import ABC, abstractmethod
+from typing import Optional, List, Type, Tuple, Dict
+import torch
+
+from flow_ODE import ODE, SDE
+from flow_models import MLPVectorField, MLPScore
+from flow_path import ConditionalProbabilityPath
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class ConditionalVectorFieldODE(ODE):
+    def __init__(self, path: ConditionalProbabilityPath, z: torch.Tensor):
+        """
+        Args:
+        - path: the ConditionalProbabilityPath object to which this vector field corresponds
+        - z: the conditioning variable, (1, dim)
+        """
+        super().__init__()
+        self.path = path
+        self.z = z
+
+    def drift_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the conditional vector field u_t(x|z)
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        bs = x.shape[0]
+        z = self.z.expand(bs, *self.z.shape[1:])
+        return self.path.conditional_vector_field(x, z, t)
+
+
+class LearnedVectorFieldODE(ODE):
+    def __init__(self, net: MLPVectorField):
+        self.net = net
+
+    def drift_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: (bs, dim)
+            - t: (bs, dim)
+        Returns:
+            - u_t: (bs, dim)
+        """
+        return self.net(x, t)
+
+
+class ConditionalVectorFieldSDE(SDE):
+    def __init__(self, path: ConditionalProbabilityPath, z: torch.Tensor, sigma: float):
+        """
+        Args:
+        - path: the ConditionalProbabilityPath object to which this vector field corresponds
+        - z: the conditioning variable, (1, ...)
+        """
+        super().__init__()
+        self.path = path
+        self.z = z
+        self.sigma = sigma
+
+    def drift_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Returns the conditional vector field u_t(x|z)
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        bs = x.shape[0]
+        z = self.z.expand(bs, *self.z.shape[1:])
+        return self.path.conditional_vector_field(
+            x, z, t
+        ) + 0.5 * self.sigma**2 * self.path.conditional_score(x, z, t)
+
+    def diffusion_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        return self.sigma * torch.randn_like(x)
+
+
+class LangevinFlowSDE(SDE):
+    def __init__(self, flow_model: MLPVectorField, score_model: MLPScore, sigma: float):
+        """
+        Args:
+        - path: the ConditionalProbabilityPath object to which this vector field corresponds
+        - z: the conditioning variable, (1, dim)
+        """
+        super().__init__()
+        self.flow_model = flow_model
+        self.score_model = score_model
+        self.sigma = sigma
+
+    def drift_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        return self.flow_model(x, t) + 0.5 * self.sigma**2 * self.score_model(x, t)
+
+    def diffusion_coefficient(self, x: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - x: state at time t, shape (bs, dim)
+            - t: time, shape (bs,.)
+        Returns:
+            - u_t(x|z): shape (batch_size, dim)
+        """
+        return self.sigma * torch.randn_like(x)
---
+++ MIT_practice/flow_path.py
@@ -0,0 +1,311 @@
+from abc import ABC, abstractmethod
+from typing import Optional, List, Type, Tuple, Dict
+import torch
+from torch.func import vmap, jacrev
+
+from flow_samples import Sampleable, Gaussian
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Alpha(ABC):
+    def __init__(self):
+        # Check alpha_t(0) = 0
+        assert torch.allclose(self(torch.zeros(1, 1)), torch.zeros(1, 1))
+        # Check alpha_1 = 1
+        assert torch.allclose(self(torch.ones(1, 1)), torch.ones(1, 1))
+
+    @abstractmethod
+    def __call__(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Evaluates alpha_t. Should satisfy: self(0.0) = 0.0, self(1.0) = 1.0.
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - alpha_t (num_samples, 1)
+        """
+        pass
+
+    def dt(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Evaluates d/dt alpha_t.
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - d/dt alpha_t (num_samples, 1)
+        """
+        t = t.unsqueeze(1)  # (num_samples, 1, 1)
+        dt = vmap(jacrev(self))(t)  # (num_samples, 1, 1, 1, 1)
+        return dt.view(-1, 1)
+
+
+class Beta(ABC):
+    def __init__(self):
+        # Check beta_0 = 1
+        assert torch.allclose(self(torch.zeros(1, 1)), torch.ones(1, 1))
+        # Check beta_1 = 0
+        assert torch.allclose(self(torch.ones(1, 1)), torch.zeros(1, 1))
+
+    @abstractmethod
+    def __call__(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Evaluates alpha_t. Should satisfy: self(0.0) = 1.0, self(1.0) = 0.0.
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - beta_t (num_samples, 1)
+        """
+        pass
+
+    def dt(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Evaluates d/dt beta_t.
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - d/dt beta_t (num_samples, 1)
+        """
+        t = t.unsqueeze(1)  # (num_samples, 1, 1)
+        dt = vmap(jacrev(self))(t)  # (num_samples, 1, 1, 1, 1)
+        return dt.view(-1, 1)
+
+
+class LinearAlpha(Alpha):
+    """
+    Implements alpha_t = t
+    """
+
+    def __call__(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - alpha_t (num_samples, 1)
+        """
+        return t
+
+    def dt(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Evaluates d/dt alpha_t.
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - d/dt alpha_t (num_samples, 1)
+        """
+        return torch.ones_like(t)
+
+
+class SquareRootBeta(Beta):
+    """
+    Implements beta_t = rt(1-t)
+    """
+
+    def __call__(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - beta_t (num_samples, 1)
+        """
+        return torch.sqrt(1 - t)
+
+    def dt(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Evaluates d/dt alpha_t.
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - d/dt alpha_t (num_samples, 1)
+        """
+        return -0.5 / (torch.sqrt(1 - t) + 1e-4)
+
+
+class ConditionalProbabilityPath(torch.nn.Module, ABC):
+    """
+    Abstract base class for conditional probability paths
+    """
+
+    def __init__(self, p_simple: Sampleable, p_data: Sampleable):
+        super().__init__()
+        self.p_simple = p_simple
+        self.p_data = p_data
+
+    def sample_marginal_path(self, t: torch.Tensor) -> torch.Tensor:
+        """
+        Samples from the marginal distribution p_t(x) = p_t(x|z) p(z)
+        Args:
+            - t: time (num_samples, 1)
+        Returns:
+            - x: samples from p_t(x), (num_samples, dim)
+        """
+        num_samples = t.shape[0]
+        # Sample conditioning variable z ~ p(z)
+        z = self.sample_conditioning_variable(num_samples)  # (num_samples, dim)
+        # Sample conditional probability path x ~ p_t(x|z)
+        x = self.sample_conditional_path(z, t)  # (num_samples, dim)
+        return x
+
+    @abstractmethod
+    def sample_conditioning_variable(self, num_samples: int) -> torch.Tensor:
+        """
+        Samples the conditioning variable z
+        Args:
+            - num_samples: the number of samples
+        Returns:
+            - z: samples from p(z), (num_samples, dim)
+        """
+        pass
+
+    @abstractmethod
+    def sample_conditional_path(self, z: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Samples from the conditional distribution p_t(x|z)
+        Args:
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - x: samples from p_t(x|z), (num_samples, dim)
+        """
+        pass
+
+    @abstractmethod
+    def conditional_vector_field(
+        self, x: torch.Tensor, z: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Evaluates the conditional vector field u_t(x|z)
+        Args:
+            - x: position variable (num_samples, dim)
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - conditional_vector_field: conditional vector field (num_samples, dim)
+        """
+        pass
+
+    @abstractmethod
+    def conditional_score(
+        self, x: torch.Tensor, z: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Evaluates the conditional score of p_t(x|z)
+        Args:
+            - x: position variable (num_samples, dim)
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - conditional_score: conditional score (num_samples, dim)
+        """
+        pass
+
+
+class GaussianConditionalProbabilityPath(ConditionalProbabilityPath):
+    def __init__(self, p_data: Sampleable, alpha: Alpha, beta: Beta):
+        p_simple = Gaussian.isotropic(p_data.dim, 1.0)
+        super().__init__(p_simple, p_data)
+        self.p_data = p_data
+        self.alpha = alpha
+        self.beta = beta
+
+    def sample_conditioning_variable(self, num_samples: int) -> torch.Tensor:
+        """
+        Samples the conditioning variable z ~ p_data(x)
+        Args:
+            - num_samples: the number of samples
+        Returns:
+            - z: samples from p(z), (num_samples, dim)
+        """
+        return self.p_data.sample(num_samples)
+
+    def sample_conditional_path(self, z: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Samples from the conditional distribution p_t(x|z) = N(alpha_t * z, beta_t**2 * I_d)
+        Args:
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - x: samples from p_t(x|z), (num_samples, dim)
+        """
+        return self.alpha(t) * z + self.beta(t) * torch.randn_like(z)
+
+    def conditional_vector_field(
+        self, x: torch.Tensor, z: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Evaluates the conditional vector field u_t(x|z)
+        Note: Only defined on t in [0,1)
+        Args:
+            - x: position variable (num_samples, dim)
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - conditional_vector_field: conditional vector field (num_samples, dim)
+        """
+        B = self.beta.dt(t) / self.beta(t)
+        A = self.alpha.dt(t) - B * self.alpha(t)
+        return A * z + B * x
+
+    def conditional_score(
+        self, x: torch.Tensor, z: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Evaluates the conditional score of p_t(x|z) = N(alpha_t * z, beta_t**2 * I_d)
+        Note: Only defined on t in [0,1)
+        Args:
+            - x: position variable (num_samples, dim)
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - conditional_score: conditional score (num_samples, dim)
+        """
+        return -((x - self.alpha(t) * z) / self.beta(t) ** 2)
+
+
+class LinearConditionalProbabilityPath(ConditionalProbabilityPath):
+    def __init__(self, p_simple: Sampleable, p_data: Sampleable):
+        super().__init__(p_simple, p_data)
+
+    def sample_conditioning_variable(self, num_samples: int) -> torch.Tensor:
+        """
+        Samples the conditioning variable z ~ p_data(x)
+        Args:
+            - num_samples: the number of samples
+        Returns:
+            - z: samples from p(z), (num_samples, ...)
+        """
+        return self.p_data.sample(num_samples)
+
+    def sample_conditional_path(self, z: torch.Tensor, t: torch.Tensor) -> torch.Tensor:
+        """
+        Samples the random variable X_t = (1-t) X_0 + tz
+        Args:
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - x: samples from p_t(x|z), (num_samples, dim)
+        """
+        X0 = self.p_simple.sample(z.shape[0])
+        return (1 - t) * X0 + t * z
+
+    def conditional_vector_field(
+        self, x: torch.Tensor, z: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Evaluates the conditional vector field u_t(x|z) = (z - x) / (1 - t)
+        Note: Only defined on t in [0,1)
+        Args:
+            - x: position variable (num_samples, dim)
+            - z: conditioning variable (num_samples, dim)
+            - t: time (num_samples, 1)
+        Returns:
+            - conditional_vector_field: conditional vector field (num_samples, dim)
+        """
+        return (z - x) / (1 - t)
+
+    def conditional_score(
+        self, x: torch.Tensor, z: torch.Tensor, t: torch.Tensor
+    ) -> torch.Tensor:
+        """
+        Not known for Linear Conditional Probability Paths
+        """
+        raise Exception("You should not be calling this function!")
